65a3004cfb95772c99f61ec024f7871d00555f9f
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-04-21 17:18:32 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ setup(
     name="PyGrammar",
     version="1.0",
     author="Mitchell Shibilski-Unkel",
-<<<<<<< Updated upstream
-    py_modules=['src.PyGrammar']
-=======
-    setup_requires=['src'],
     py_modules=['PyGrammar'],
     package_dir={'src'},
->>>>>>> Stashed changes
 )
