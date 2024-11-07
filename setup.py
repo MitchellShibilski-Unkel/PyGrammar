2a04d9651fe86afd659f7b665e03c36fc484fe62
Mitchell Shibilski-Unkel <133730558+MitchellShibilski-Unkel@users.noreply.github.com>
2024-11-07 11:01:53 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     url="https://github.com/MitchellShibilski-Unkel/PyGrammar.git",
     packages=setuptools.find_packages(),
     #install_requires=['transformers', 'sentencepiece==0.1.95', 'python-Levenshtein==0.12.2', 'fuzzywuzzy==0.18.0',  'tokenizers==0.10.2', 'fsspec==2021.5.0', 'lm-scorer==0.4.2', 'errant'],
-    install_requires=['transformers', 'sentencepiece', 'python-Levenshtein', 'fuzzywuzzy',  'tokenizers', 'fsspec', 'errant', 'torch'],
+    install_requires=['nltk', 'language_tool_python'],
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "License :: MIT",
